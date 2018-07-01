286cd72845a8a5afe478ad63d95ae687cb8c7a19
dgljw <lijianwen1@gd.chinamobile.com>
2018-07-01 10:00:28 +0800

    2018-07-01 10:00

--- src/main/java/com/cn/techblogs/controller/IndexSearchController.java
+++ src/main/java/com/cn/techblogs/controller/IndexSearchController.java
@@ -6,14 +6,28 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.ResponseBody;
+ 
 import com.cn.techblogs.pojo.User;
+
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
 
 @Controller
 @RequestMapping("/index")
+@ResponseBody
 public class IndexSearchController {
 	@RequestMapping("/search")
-	public String Search(HttpServletRequest request,Model model){
-		return "Just a try";
+	public String DoSearch(HttpServletRequest request,Model model){
+		JSONArray array =new JSONArray();
+
+        for(int i = 0; i < 10; i++){
+            JSONObject obj=new JSONObject();
+            //前台通过key值获得对应的value值
+            obj.put("Title", "Book " + i);
+            obj.put("Name", "Amaze");
+            array.add(obj);
+        }
+       return array.toString();
 	}
 }
